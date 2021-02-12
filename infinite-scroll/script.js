3de1c075397079d14a47b4bc7ccc93ff7dd8bec1
Rafal Wojcik <Rafal.Wojcik1@gds.ey.com>
2021-02-12 17:32:52 +0100

    spinner for initial load added

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -1,20 +1,35 @@
-
 const imgContainer = document.getElementById('img-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
+let initialLoad = true;
 
-const photoCount = 10;
+let photoCount = 5;
 const apiKey = 'uVqRIcM4eEEzQFmoBmTGe8epZ-HXaF4S49cCqTYH7tc';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${photoCount}`;
 
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        initialLoad = false;
+        photoCount = 30;
+    }
+}
+
 function setAtrributesHelper(element, attributes) {
-    for (let key in attributes) {
+    for (const key in attributes) {
         element.setAttribute(key, attributes[key]);
     }
 }
 
 function createImagesDomCollection() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     photosArray.forEach((photo) => {
         // Create <a></a> tag
         const item = document.createElement('a');
@@ -31,6 +46,9 @@ function createImagesDomCollection() {
             title: photo.alt_description
         })
 
+        // Event Listener, check when each is finished loading
+        image.addEventListener('load', imageLoaded);
+
         // Create elements collection
         item.appendChild(image);
         imgContainer.appendChild(item);
@@ -41,12 +59,18 @@ async function getPhotosFormUnsplashApi() {
     try {
         const response = await fetch(apiUrl);
         photosArray = await response.json();
-        console.log(photosArray);
-
         createImagesDomCollection();
     } catch (error) {
         
     }
 }
+
+// Check to see if scrolling near bottom of page, then load more photos
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotosFormUnsplashApi();
+    }
+})
 
 getPhotosFormUnsplashApi();
